f4e7271df37da1a7247e9ef437a0731ead7ba796
agungtsmn <0812agung@gmail.com>
2022-03-18 10:35:53 +0700

    Percobaan Array 2 Dimensi

--- src/main/kotlin/4_Array 2 Dimensi.kt
+++ src/main/kotlin/4_Array 2 Dimensi.kt
@@ -5,7 +5,7 @@ fun main() {
     for (i in 0..4) {
         var array = arrayOf<Int>()
         for (j in 0..4) {
-            array += 1
+            array += 0
         }
         cinema += array
     }
@@ -20,11 +20,11 @@ fun main() {
 
 
     cinema[2][2] = 1
-    for (i in 1..3) {
+    for (i in 1..3) { // mengulang untuk array index ke 1 - 3
         cinema[3][i] = 1 // memberikan nilai 1 pada isi array ke 3
     }
-    for (i in 0..4) {
-        cinema[4][i] = 2 // memberikan nilai 2 pada isi array ke 4
+    for (i in 0..4) { // mengulang untuk array index ke 0 - 4
+        cinema[4][i] = 1 // memberikan nilai 1 pada isi array ke 4
     }
     println()
     for (array in cinema) {
