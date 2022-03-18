f669f3c176926a401bea58283aeb66b3df6473dd
agungtsmn <0812agung@gmail.com>
2022-03-18 10:30:57 +0700

    Percobaan Array 2 Dimensi

---
+++ src/main/kotlin/4_Array 2 Dimensi.kt
@@ -0,0 +1,37 @@
+fun main() {
+
+    // menentukan isi untuk variabel cinema
+    var cinema = arrayOf<Array<Int>>()
+    for (i in 0..4) {
+        var array = arrayOf<Int>()
+        for (j in 0..4) {
+            array += 1
+        }
+        cinema += array
+    }
+
+    // perulangan di dalam perulangan
+    for (array in cinema) {
+        for (value in array) {
+            print("$value ")
+        }
+        println()
+    }
+
+
+    cinema[2][2] = 1
+    for (i in 1..3) {
+        cinema[3][i] = 1 // memberikan nilai 1 pada isi array ke 3
+    }
+    for (i in 0..4) {
+        cinema[4][i] = 2 // memberikan nilai 2 pada isi array ke 4
+    }
+    println()
+    for (array in cinema) {
+        for (value in array) {
+            print("$value ")
+        }
+        println()
+    }
+
+}
